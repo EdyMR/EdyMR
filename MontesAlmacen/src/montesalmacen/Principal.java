58406dd140b7668812529aa60584cc9dabbb2d00
EdyMR <94832961+EdyMR@users.noreply.github.com>
2022-03-19 20:56:10 +0100

    Versión 1.0.1
    Prueba de la versión 1.0.1

--- MontesAlmacen/src/montesalmacen/Principal.java
+++ MontesAlmacen/src/montesalmacen/Principal.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Principal {
 
-//Prueba para GitHub
+//Prueba para el cliente Desktop
 
     public static void main(String[] args) {
         Almacen almacen = new Almacen();
